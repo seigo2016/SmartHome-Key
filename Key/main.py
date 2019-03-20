98bcd257eb4b8e16b97858894f85605971428ec3
seigo2016 <2003seigo@gmail.com>
2019-03-20 20:14:14 +0900

    Update main.py

--- Key/main.py
+++ Key/main.py
@@ -5,23 +5,7 @@ import socket
 GPIO.setmode(GPIO.BCM)
 flag = True
 out3 = 4
-out4 = 17
-GPIO.setup(out4,GPIO.OUT)
 GPIO.setup(out3,GPIO.IN)
-def open():
-    for i in range(0,50,1):
-        servo1.ChangeDutyCycle(7.5-i/10)
-        servo2.ChangeDutyCycle(7.5-i/10)
-        time.sleep(0.01)
-    servo1.start(0)
-    servo2.start(0)
-def close():
-    for i in range(0,50,1):
-        servo1.ChangeDutyCycle(2.5+i/10)
-        servo2.ChangeDutyCycle(2.5+i/10)
-        time.sleep(0.01)
-    servo1.start(0)
-    servo2.start(0)
 try:
     while True:
         if GPIO.input(out3) == GPIO.HIGH and not flag:
